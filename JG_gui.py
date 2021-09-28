eb63ddd993a99e4b7e2ee06241ef657f689f4196
spet-d <solteszpeter98@gmail.com>
2021-09-28 21:35:15 +0200

    Egyszeru fajlok
    Csak létrehoztam két fájlt amiben dolgozhatunk majd. Eggyet a GUI-nak és eggyet a 'main' függvénynek amibol meghivunk majd mindent

---
+++ JG_gui.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from tkinter import ttk
+
+# A kezelo felulet osztalya
+class felulet:
+    
+    def __init__(self, root):
+        
+        root.title("JGtimer")
+        # TODO...
---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from JG_gui import *
+
+def main():
+    root = Tk()
+    felulet(root)
+    root.mainloop()
+
+
+##########################
+if __name__ == '__main__':
+    main()
