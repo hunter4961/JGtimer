f5d32bef0f2e943b375ef580e89e554621011363
spet-d <solteszpeter98@gmail.com>
2021-10-02 00:41:34 +0200

    Takarítás
    Az ahogy a korábbi kiosztás kinézett nem lett volna működő képes, ezért rakom most fel ezt az üres vázat. Így legalább egy helyről indulunk.

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 
-from JG_gui import *
-
-def main():
-    root = Tk()
-    felulet(root)
-    root.mainloop()
+from tkinter import *
+from tkinter import ttk
 
 
-##########################
+class MainApplication(Frame):
+
+    def __init__(self, master):
+        self.master = master
+        ttk.Frame.__init__(self, self.master)
+        self.configure_gui()
+        self.create_widgets()
+
+
+    def congigure_gui(self):
+    # ...
+
+    def create_widgets(self):
+    # ...
+
 if __name__ == '__main__':
-    main()
+    root = Tk()
+    main_app = MainApplication(root)
+    root.mainloop()
